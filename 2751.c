57e60174ffe183c7dc75aab969fc813d28b20604
Geon <49513356+FunFunGun@users.noreply.github.com>
2023-03-10 14:10:33 +0900

    수 정렬하기 2

--- 2751.c
+++ 2751.c
@@ -1,7 +1,8 @@
+//퀵 정렬 라이브러리 참고 - c언어로 쉽게 풀어쓴 자료구조 p.478
+
 #include <stdio.h>
 #include <stdlib.h>
 
-//퀵 정렬 라이브러리 참고 - c언어로 쉽게 풀어쓴 자료구조 p.478
 int compare(const void *index1, const void *index2)
 {
     if(*(int *)index1 > *(int *)index2)
@@ -24,7 +25,7 @@ int main()
     for(int i=0; i<N; i++)
         scanf("%d", &arr[i]);
         
-    qsort((void *)arr, (size_t)N, sizeof(int), compare);
+    qsort((void *)arr, (size_t)N, sizeof(int), compare);//퀵 정렬 라이브러리
     
     for(int i=0; i<N; i++)
         printf("%d\n", arr[i]);
