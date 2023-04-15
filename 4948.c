7949765c080189060e439db13b099c5da85b3303
Geon Kim <49513356+funfungun@users.noreply.github.com>
2023-04-15 23:48:36 +0900

    베르트랑 공준

---
+++ 4948.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+//에라토스테네스의 체
+int main()
+{
+    int N = 1000000;
+    int arr[1000001] = {0,};
+    
+    for(int i=2; i<=N; i++)
+        arr[i] = i;
+        
+    for(int i=2; i<=N; i++)
+    {
+        if(arr[i] == 0)
+            continue;
+            
+        for(int j=i+i; j<=N; j+=i)
+            arr[j] = 0;
+    }
+    
+    while(1)
+    {
+        int n;
+        scanf("%d", &n);
+        
+        if(n == 0)
+            break;
+        
+        int count = 0;
+        
+        //n보다 크고 2*n보다 작거나 같은 소수의 개수
+        for(int i=n+1; i<=2*n; i++)
+        {
+            if(arr[i] != 0)
+                count++;
+        }
+        
+        printf("%d\n", count);
+    }
+    
+    return 0;
+}
