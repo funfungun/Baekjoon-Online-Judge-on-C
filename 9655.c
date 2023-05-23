5a297094b1318e9555f90e1e3f9a14d0d67a36e6
Geon Kim <49513356+funfungun@users.noreply.github.com>
2023-05-23 15:53:05 +0900

    돌 게임

---
+++ 9655.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    
+    if(N%2 == 1)
+        printf("SK");
+        
+    else
+        printf("CY");
+        
+    return 0;
+}
